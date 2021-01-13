abaed203345fef5705d74d47b4b7927930381621
Franziska Kuesters <franziska.kuesters@smail.inf.h-brs.de>
2021-01-13 12:06:59 +0100

    index.html and nano set up

--- public/nodejs_server.js
+++ public/nodejs_server.js
@@ -3,8 +3,10 @@ const http = require('http')
 const cors = require('cors');
 const ini = require('ini');
 const fs = require('fs');
+const path = require('path')
 
-const config = ini.parse(fs.readFileSync('./configuration.ini', 'utf-8'))
+
+const config = ini.parse(fs.readFileSync('../configuration.ini', 'utf-8'))
 const username = config.remote.user;
 const password = config.remote.password;
 const host = config.remote.host;
@@ -23,6 +25,25 @@ const local_port = 3131;
 app.listen(local_port, () =>
   console.log(`App is listening on port ${local_port}.`)
 );
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './index.html'))
+})
+
+const couch = require('nano')('http://' + username + ":" + password + "@" + host + ":" + port)
+//const db = couch.db.create("cert-store", (err, data) => {
+  //console.log(data.toString());
+  //console.log(err.toString());
+//});
+
+const certstore = couch.db.use('cert-store');
+async function asyncCall() {
+  const dblist = await couch.db.list();
+  console.log(dblist.toString());
+}
+asyncCall();
+
+
 
 
 app.get('/', async (req, res) => {
