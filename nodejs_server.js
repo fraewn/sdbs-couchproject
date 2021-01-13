90e3ab03bbb7c4c74a17c4a01e05e9ba55990b1a
Franziska Kuesters <franziska.kuesters@smail.inf.h-brs.de>
2021-01-13 13:18:23 +0100

    back to the roots

--- nodejs_server.js
+++ nodejs_server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const http = require('http')
 const cors = require('cors');
 const path = require('path')
+const fs = require('fs');
+const ini = require('ini');
+
+const config = ini.parse(fs.readFileSync('./configuration.ini', 'utf-8'))
+const username = config.remote.user;
+const password = config.remote.password;
+const host = config.remote.host;
+const port = config.remote.port;
 
 const app = express();
 
@@ -19,6 +27,56 @@ app.listen(local_port, () =>
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.listen(port, () => {
+  console.log('Example app listening at http://localhost:${port}')
+})
+
+
+// database operations
+
+console.log("userhello" + username+ password + host + port);
+
+const couch = require('nano')('http://' + username + ":" + password + "@" + host + ":" + port)
+
+
+// create database "cert-store"
+//const db = couch.db.create("cert-store", (err, data) => {
+//console.log(data.toString());
+//console.log(err.toString());
+//});
+
+
+const certstore = couch.db.use('cert-store');
+
+async function asyncCall() {
+  const dblist = await couch.db.list();
+  console.log(dblist.toString());
+}
+
+asyncCall();
+
+app.get('/', async (req, res) => {
+
+  http.get('http://' + username + ":" + password + "@" + host + ":" + port + "/_uuids", (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    resp.on('end', () => {
+      console.log(data);
+      res.send(data);
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+
+//          res.send("hi");
 })
 
 
